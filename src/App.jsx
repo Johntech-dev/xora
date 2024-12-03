25b3762644181e98e7e9c1559caf18456d86cd9a
Johntech-dev <johnayomide50@gmail.com>
2024-12-03 23:10:49 +0100

    header-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './sections/Header.jsx';
+import Hero from './sections/Hero.jsx';
 
 const App = () => {
   return (
     <div>
         <Header />
+        <Hero />
     </div>
   )
 }
